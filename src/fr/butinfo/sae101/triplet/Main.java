2fbc43b613c1712c97d476546e0e7318ca476448
Louis Karamucki <louis_karamucki@ens.univ-artois.fr>
2023-10-23 10:52:15 +0200

    Ajout test multiply dans Main

--- src/fr/butinfo/sae101/triplet/Main.java
+++ src/fr/butinfo/sae101/triplet/Main.java
@@ -5,12 +5,15 @@ public class Main {
         Triplet t1 = new Triplet(1.0, 2.0, 3.0);
         Triplet t2 = new Triplet(4.0, 5.0, 6.0);
 
-        Triplet addition = t1.add(t2);
+        Triplet add = t1.add(t2);
         Triplet sub = t2.sub(t1);
+        Triplet mult_sca = t1.multiply(2.0);
+
         System.out.println(t1);
         System.out.println(t2);
 
-        System.out.println(addition);
-        System.out.println(sub);
+        System.out.println("Addition: " + add);
+        System.out.println("Soustraction: " + sub);
+        System.out.println("Multiplication: " + mult_sca);
     }
 }
