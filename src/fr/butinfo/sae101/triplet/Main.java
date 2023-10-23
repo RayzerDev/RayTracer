f6ece8a0d4366b14894bdb51fe94c59ea6129629
Louis Karamucki <louis_karamucki@ens.univ-artois.fr>
2023-10-23 10:41:49 +0200

    Ajout test add dans Main

--- src/fr/butinfo/sae101/triplet/Main.java
+++ src/fr/butinfo/sae101/triplet/Main.java
@@ -1,4 +1,13 @@
 package fr.butinfo.sae101.triplet;
 
 public class Main {
+    public static void main(String[] args) {
+        Triplet t1 = new Triplet(1.0, 2.0, 3.0);
+        Triplet t2 = new Triplet(4.0, 5.0, 6.0);
+
+        Triplet addition = t1.add(t2);
+        System.out.println(t1);
+        System.out.println(t2);
+        System.out.println(addition);
+    }
 }
