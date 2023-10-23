ea6db13c098a7418262208d6d68bbdbd9d02bb69
Louis Karamucki <louis_karamucki@ens.univ-artois.fr>
2023-10-23 10:47:47 +0200

    Ajout test sub dans Main

--- src/fr/butinfo/sae101/triplet/Main.java
+++ src/fr/butinfo/sae101/triplet/Main.java
@@ -6,8 +6,11 @@ public class Main {
         Triplet t2 = new Triplet(4.0, 5.0, 6.0);
 
         Triplet addition = t1.add(t2);
+        Triplet sub = t2.sub(t1);
         System.out.println(t1);
         System.out.println(t2);
+
         System.out.println(addition);
+        System.out.println(sub);
     }
 }
