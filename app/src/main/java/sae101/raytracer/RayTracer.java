df329e85c7ed8b10b518028c2d6c74ad450ac983
Anthony Rolland <anthony_rolland@ens.univ-artois.fr>
2023-10-24 12:46:16 +0200

    Ajout d'un try et catch dans la méthode view()

--- app/src/main/java/sae101/raytracer/RayTracer.java
+++ app/src/main/java/sae101/raytracer/RayTracer.java
@@ -5,7 +5,11 @@ import sae101.parser.scene.Scene;
 import sae101.triplet.Point;
 import sae101.triplet.Vector;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class RayTracer {
     private Scene scene;
@@ -36,6 +40,14 @@ public class RayTracer {
                 Vector d = camera.getU().multiply(a).add(camera.getV().multiply(b)).normalize();
                 Point p = camera.getLookFrom() + d * t;
             }
+        }
+        try {
+            BufferedImage image= view();
+            File outputFile = new File("outpout.png");
+            ImageIO.write(image, "png", outputFile);
+        }
+        catch (IOException e){
+            System.err.println("Erreur lors de la création du fichier : "+e.getMessage());
         }
     }
 }
