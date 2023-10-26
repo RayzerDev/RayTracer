18a5e984943e1096b32fc0d65ba8a43b4ed9f7e8
Damien Allaert <damien_allaert@ens.univ-artois.fr>
2023-10-26 10:18:31 +0200

    Ajout de la methode de calcul de Blinn-Phong

---
+++ app/src/main/java/sae101/calculColor/BlinnPhongColorCal.java
@@ -0,0 +1,13 @@
+package sae101.calculColor;
+
+import sae101.parser.objects.Sphere;
+import sae101.parser.scene.Scene;
+import sae101.triplet.Color;
+import sae101.triplet.Point;
+
+public class BlinnPhongColorCal implements IFormLambert{
+    @Override
+    public Color calculateColor(Sphere sphere, Scene scene, Point p) {
+        return null;
+    }
+}
