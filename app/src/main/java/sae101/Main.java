dcccfaa39cffb86c89f5dc4ad7d65df1c0dc223b
Anthony Rolland <anthony_rolland@ens.univ-artois.fr>
2023-10-26 12:03:52 +0200

    :zap: Optimisaiton du code avec SonarLint dans le main

--- app/src/main/java/sae101/Main.java
+++ app/src/main/java/sae101/Main.java
@@ -2,7 +2,6 @@ package sae101;
 
 import sae101.parser.Parser;
 import sae101.parser.scene.Scene;
-import sae101.parser.scene.SceneObjects;
 import sae101.raytracer.RayTracer;
 
 import java.io.IOException;
@@ -20,7 +19,7 @@ public class Main {
      */
     public static void main(String[] args) throws IOException {
         Path resourceDirectory = Paths.get("src","main","resources","figures");
-        String path = resourceDirectory.toFile().getAbsolutePath() + "/" + args[0];
+        String path = "%s/%s".formatted(resourceDirectory.toFile().getAbsolutePath(), args[0]);
         Parser pars;
         pars = new Parser(path);
         Scene scene = pars.build();
